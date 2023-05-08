b51978eec84c354d291f4ffce0f482d3e74715f7
Rifqy232 <rachmanmry20d@student.unhas.ac.id>
2023-05-09 01:04:54 +0800

    Create Banner

--- app/src/main/java/com/mry/jetcoffeeapp/MainActivity.kt
+++ app/src/main/java/com/mry/jetcoffeeapp/MainActivity.kt
@@ -3,14 +3,23 @@ package com.mry.jetcoffeeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mry.jetcoffeeapp.ui.components.SearchBar
 import com.mry.jetcoffeeapp.ui.theme.JetCoffeeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +35,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun JetCoffeeApp() {
+    Column {
+        Banner()
+    }
+}
 
+@Composable
+fun Banner(
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier = modifier) {
+        Image(
+            painter = painterResource(id = R.drawable.banner),
+            contentDescription = "Banner Image",
+            contentScale = ContentScale.Crop,
+            modifier = modifier.height(160.dp)
+        )
+        SearchBar()
+    }
 }
 
 @Preview(showBackground = true, device = Devices.PIXEL_4)
